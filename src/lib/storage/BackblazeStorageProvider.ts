171c8f83fcf8bd0f3d9b86457f05286d38746e52
GFrasson <bielfc27@gmail.com>
2026-04-03 21:58:24 -0300

    feat: Initialize variables

--- src/lib/storage/BackblazeStorageProvider.ts
+++ src/lib/storage/BackblazeStorageProvider.ts
@@ -5,19 +5,17 @@ import crypto from 'crypto';
 
 export class BackblazeStorageProvider implements IStorageProvider {
   private s3: S3Client;
-  private bucketName: string;
-  private signedUrlExpiresInSeconds: number;
+  private bucketName = process.env.BACKBLAZE_BUCKET_NAME ?? '';
+  private bucketFolder = process.env.BACKBLAZE_BUCKET_FOLDER ?? 'wedding_capsule';
+  private signedUrlExpiresInSeconds= Number(process.env.BACKBLAZE_SIGNED_URL_EXPIRES_IN_SECONDS ?? '300');
 
   constructor() {
-    this.bucketName = process.env.BACKBLAZE_BUCKET_NAME || '';
-    this.signedUrlExpiresInSeconds = Number(process.env.BACKBLAZE_SIGNED_URL_EXPIRES_IN_SECONDS || '300');
-    
     this.s3 = new S3Client({
-      endpoint: process.env.BACKBLAZE_ENDPOINT || '', // e.g., https://s3.us-west-000.backblazeb2.com
-      region: process.env.BACKBLAZE_REGION || 'us-west-000',
+      endpoint: process.env.BACKBLAZE_ENDPOINT ?? '', // e.g., https://s3.us-west-000.backblazeb2.com
+      region: process.env.BACKBLAZE_REGION ?? 'us-west-000',
       credentials: {
-        accessKeyId: process.env.BACKBLAZE_KEY_ID || '',
-        secretAccessKey: process.env.BACKBLAZE_APPLICATION_KEY || '',
+        accessKeyId: process.env.BACKBLAZE_KEY_ID ?? '',
+        secretAccessKey: process.env.BACKBLAZE_APPLICATION_KEY ?? '',
       },
       // Force path style is occasionally needed for some S3 compatible APIs
       // forcePathStyle: true, 
@@ -27,7 +25,7 @@ export class BackblazeStorageProvider implements IStorageProvider {
   async upload(buffer: Buffer, originalFilename: string, mimeType: string): Promise<UploadResult> {
     const extension = originalFilename.split('.').pop();
     const uniqueName = crypto.randomUUID() + (extension ? `.${extension}` : '');
-    const key = `wedding_capsule/${uniqueName}`;
+    const key = `${this.bucketFolder}/${uniqueName}`;
 
     const command = new PutObjectCommand({
       Bucket: this.bucketName,
